c6ddb4e1dd55163a044b7cefa50360a7141a0a0a
Daina Slezaite <daina.slezaite@gmail.com>
2021-03-12 08:27:49 +0100

    add console.logs for debugging purposes

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -10,26 +10,33 @@ authRoutes.post('/signup', (req, res, next) => {
   const password = req.body.password;
   const description = req.body.description;
 
+  console.log('This is req.body: ', req.body);
+
   if(!email || !username || !password) {
     res.status(400).json({message: 'All fields - email, username and password - must be provided to sign up.'});
+    console.log('All fields provided during signup');
     return;
   }
 
   const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/;
   if(!regex.test(password)) {
     res.status(400).json({message: 'Password must contain a number, lowercase and uppercase letters and be at least 8 characters long.'});
+    console.log('Password passes regex test');
     return;
   }
 
   User.findOne({email}, (err, foundUser) => {
+    console.log('Searching for user here');
 
     if(err) {
       res.status(500).json({message: 'Something went wrong with the email check.'});
+      console.log('There has been an error with the email check:' , err)
       return;
     }
 
     if(foundUser) {
       res.status(400).json({message: 'This email is already taken. Try with another one.'});
+      console.log('This email is already taken by:', foundUser)
       return;
     }
 
@@ -44,15 +51,20 @@ authRoutes.post('/signup', (req, res, next) => {
     });
 
     newUser.save(err => {
+      console.log('Trying to save new user');
       if(err) {
         res.status(500).json({message: 'Something went wrong while saving the user to the database.'});
+        console.log('Could not save you: ', err)
         return;
       }
 
       req.login(newUser, (err) => {
 
+        console.log('trying to log in after signup');
+
         if(err) {
           res.status(500).json({message: 'Unsuccessful login after signup.'});
+          console.log('something went wrong with login after signup:', err)
           return;
         }
         
